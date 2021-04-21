fcf135a06a80b9cdb5429d3a4b30a27d1040f7bc
Daniel-1105 <82467131+Daniel-1105@users.noreply.github.com>
2021-04-21 09:31:55 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-let answer = 0
-answer = 0
-answer = 0
-answer = 0
+let Firstnumber = 2
+let Secondnumber = 3
+let Thirdnumber = 4
+let Fourthnumber = 4
+let answer = Firstnumber + (Secondnumber + Thirdnumber * Fourthnumber)
 game.splash("it is " + answer)
